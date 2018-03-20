e5f938899aac59df3c4454bb964ce7e7bbfd9833
Brian Wandell <wandell@stanford.edu>
2018-03-19 23:07:13 -0700

    Utility to just run the examples, whether they are in a file or in a directory.

---
+++ RunExamples.m
@@ -0,0 +1,43 @@
+function [names,status] = RunExamples(str,varargin)
+% Invoke examples for all files in a directory or a function
+%
+%   [names,status] = RunExamples(fileOrDirectory, ...);
+%
+% Copyright ISETBIO Team, 2018
+%
+% See also
+%   ExecuteExamplesInFunction, ExecuteExamplesInDirectory, PrintExamples
+
+% Examples:
+%{
+  [names,status] = RunExamples('opticsGet.m','findflag',true,'printflag',true);
+  [names,status] = RunExamples('opticsGet.m','findflag',true,'printflag',false);
+%}
+% Examples:
+%{
+  directory = fullfile(isetbioRootPath,'isettools','opticalimage');
+  [names,status] = RunExamples(directory,'findflag',true,'printflag',false);
+%}
+
+%%
+p = inputParser;
+p.addRequired('str',@ischar);
+p.addParameter('printflag',false,@islogical);
+p.addParameter('findflag',true,@islogical);
+
+p.parse(str,varargin{:});
+pFlag = p.Results.printflag;
+fFlag = p.Results.findflag;
+
+%%
+if exist(str,'dir')
+    [names,status] = ExecuteExamplesInDirectory(str);
+elseif exist(str,'file')
+    names  = str;
+    status = ExecuteExamplesInFunction(str,'findfunction',fFlag,'printexampletext',pFlag);
+else
+    error('No file or directory %s\n',str);
+end
+
+end
+
